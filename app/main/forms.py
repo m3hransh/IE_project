aadaebb9bdab0309469a31b2e1adf855e59e51a7
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-02-21 12:30:15 +0330

    adding Registration

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import Employee
 
-class AdminLogin(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[
+        DataRequired(),
+        Length(1, 64),
+        Regexp(r'^[A-za-z]+$', 0, 'First Name must have only letters')])
+
+    last_name = StringField('Last Name', validators=[
+        DataRequired(),
+        Length(1, 64),
+        Regexp(r'^[A-za-z]+$', 0, 'Last Name must have only letters')])
+    national_id = StringField('National ID', validators=[
+        DataRequired(),
+        Regexp(r'^\d{10}$', 0, 'This should be 10 digits')
+    ])
+    phone_number = StringField('Phone Number', validators=[
+        DataRequired(),
+        Length(1, 20),
+        Regexp('^\d+$', 0, 'Phone Number should be numbers')])
+    address = StringField('Address', validators=[DataRequired()])
+    age = StringField('Age', validators=[
+        DataRequired(),
+        Regexp(r'^\d{1,3}$', 0, Regexp(
+            'Age should be valid number'))
+    ])
+    income = StringField('Income', validators=[
+        DataRequired(),
+        Regexp('^\d+$')
+    ])
+    married = BooleanField('Married')
     submit = SubmitField('Submit')
+
+    def validate_national_id(self, field):
+        if Employee.query.filter_by(national_id=field.data).first():
+            raise ValidationError('National ID is already registered.')
+    
+    def validate_phone_number(self, field):
+        if Employee.query.filter_by(phone_number=field.data).first():
+            raise ValidationError('Phone number is already registered.')
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,34 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 from . import main
 from .. import db
 from ..models import Employee
+from .forms import RegistrationForm
 from flask_login import login_required
 
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    
     return render_template('index.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+@login_required
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        employee = Employee(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            national_id=form.national_id.data,
+            phone_number=form.phone_number.data,
+            address=form.address.data,
+            married=form.married.data,
+            age=form.age.data,
+            income=form.income.data
+        )
+        db.session.add(employee)
+        db.session.commit()
+        flash('Employee has added successfuly.')
+        return redirect(url_for('main.register'))
+    return render_template('register.html', form=form)
